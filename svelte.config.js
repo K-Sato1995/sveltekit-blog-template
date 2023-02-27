1bb7d0d2fa687d8711fe57a948b28e4b4d0f1762
K-Sato1995 <katsuki_sato0130@icloud.com>
2023-02-27 10:41:36 +0900

    Update depedencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 import { resolve } from "path";
 
 const config = {
@@ -14,7 +14,7 @@ const config = {
 		},
 	},
 	kit: {
-		adapter: vercel()
+		adapter: adapter()
 	},
 };
 
